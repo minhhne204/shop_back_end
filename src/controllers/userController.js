e4b17b4331acf20301d0c627a02dc7d1f29f4e88
linhnvph35998 <linhnvph35998@gmail.com>
2025-11-18 22:27:01 +0700

    api đăng nhập + cập nhật + xóa và lấy thông tin người dùng

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,6 +30,109 @@ const createUser = async (req, res) => {
   }
 };
 
+
+const loginUser = async (req, res) => {
+  try {
+    const { username, email, password, confirmPassword } = req.body;
+    const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const isCheckEmail = reg.test(email);
+    if (!username || !email || !password || !confirmPassword) {
+      return res.status(200).json({
+        status: "error",
+        message: "The input is required.",
+      });
+    } else if (!isCheckEmail) {
+      return res
+        .status(200)
+        .json({ status: "error", message: "The input is email." });
+    } else if (password !== confirmPassword) {
+      return res
+        .status(200)
+        .json({ status: "error", message: "The input is password." });
+    }
+    console.log("isCheckEmail", isCheckEmail);
+    const response = await userService.loginUser();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error." });
+  }
+};
+
+
+const updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const data = req.body;
+    if (!userId) {
+      return res
+        .status(200)
+        .json({ status: "error", message: "User ID is required." });
+    }
+    const response = await userService.updateUser(userId, data);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error." });
+  }
+};
+
+
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    
+    if (!userId) {
+      return res
+        .status(200)
+        .json({ status: "error", message: "User ID is required." });
+    }
+    const response = await userService.deleteUser(userId);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error." });
+  }
+};
+
+
+const getAllUser = async (req, res) => {
+  try {
+    const response = await userService.getAllUser();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error." });
+  }
+};
+
+
+const getDetail = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    if (!userId) {
+      return res
+        .status(200)
+        .json({ status: "error", message: "User ID is required." });
+    }
+    const response = await userService.getDetail(userId);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error." });
+  }
+};
+
 module.exports = {
   createUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+  getAllUser,
+  getDetail,
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,7 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const { genneralAccessToken } = require("./jwtService");
+
 const createUser = (newUser) => {
   return new Promise(async (resolve, reject) => {
     const { username, email, password, confirmPassword } = newUser;
@@ -7,11 +10,12 @@ const createUser = (newUser) => {
       if (checkUser !== null) {
         resolve({ status: "ok", message: "The email is already" });
       }
+      const hash = await bcrypt.hash(password, 10);
       const createdUser = await User.create({
         username,
         email,
-        password,
-        confirmPassword,
+        password: hash,
+        confirmPassword: hash,
       });
       if (createdUser) {
         resolve({
@@ -25,6 +29,130 @@ const createUser = (newUser) => {
     }
   });
 };
+
+const loginUser = (userLogin) => {
+  return new Promise(async (resolve, reject) => {
+    const { username, email, password, confirmPassword } = userLogin;
+    try {
+      const checkUser = await User.findOne({ email: email });
+      if (checkUser === null) {
+        resolve({ status: "ok", message: "The user is not defined" });
+      }
+      const comparePassword = await bcrypt.compareSync(
+        password,
+        checkUser.password
+      );
+
+      if (!comparePassword) {
+        resolve({ status: "ok", message: "The password is incorrect" });
+      }
+      const access_Token = await genneralAccessToken({
+        id: checkUser._id,
+        isAdmin: checkUser.isAdmin,
+      });
+
+      const refresh_Token = await genneralRefreshToken({
+        id: checkUser._id,
+        isAdmin: checkUser.isAdmin,
+      });
+      resolve({
+        status: "ok",
+        message: "Login user successfully",
+        access_Token,
+        refresh_Token,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const updateUser = (id, data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkUser = await User.findOne({ id: id });
+      if (checkUser === null) {
+        resolve({ status: "ok", message: "The user is not defined" });
+      }
+      const updatedUser = await User.findByIdAndUpdate(id, data, { new: true });
+      if (!updatedUser) {
+        resolve({ status: "error", message: "User not found" });
+      } else {
+        resolve({
+          status: "ok",
+          message: "User updated successfully",
+          data: updatedUser,
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const deleteUser = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkUser = await User.findOne({ id: id });
+      if (checkUser === null) {
+        resolve({ status: "ok", message: "The user is not defined" });
+      }
+
+      const deletedUser = await User.findByIdAndDelete(id);
+      if (!deletedUser) {
+        resolve({ status: "error", message: "User not found" });
+      } else {
+        resolve({
+          status: "ok",
+          message: "User deleted successfully",
+          data: deletedUser,
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const getAllUser = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const users = await User.find();
+      resolve({
+        status: "ok",
+        message: "Get all users successfully",
+        data: users,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const getDetail = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        resolve({ status: "error", message: "User not found" });
+      } else {
+        resolve({
+          status: "ok",
+          message: "Get user detail successfully",
+          data: user,
+        });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
 module.exports = {
   createUser,
+  loginUser,
+  updateUser,
+  deleteUser,
+  getAllUser,
+  getDetail,
 };
